277ec74bfe803342cf85b04cef53d079e51afbab
Kevin May <pair+kevin@pivotallabs.com>
2015-04-19 21:30:33 -0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 #Admins
-Admin.create!(email: "admin@mezzweststatetour.com", password: "mezzwest", super_admin: true)
+Admin.first_or_create!(email: "admin@mezzweststatetour.com", password: "mezzwest", super_admin: true)
 
 #Levels
-pro = Level.create!(name: "Pro", games_required: 9)
-semi_pro = Level.create!(name: "Semi-Pro", games_required: 8)
-open = Level.create!(name: "Open", games_required: 7)
+pro = Level.first_or_create!(name: "Pro", games_required: 9)
+semi_pro = Level.first_or_create!(name: "Semi-Pro", games_required: 8)
+open = Level.first_or_create!(name: "Open", games_required: 7)
 
 #Players
-Player.create!(membership_identifier: 10000, first_name: "Kevin", last_name: "May", display_name: "Spartacus", level: open)
-Player.create!(membership_identifier: 10001, first_name: "Beau", last_name: "Runningen", display_name: "Beau R.", level: semi_pro)
-Player.create!(membership_identifier: 10002, first_name: "Oscar", last_name: "Dominguez", display_name: "Oscar", level: pro)
+Player.first_or_create!(membership_identifier: 10000, first_name: "Kevin", last_name: "May", display_name: "Spartacus", level: open)
+Player.first_or_create!(membership_identifier: 10001, first_name: "Beau", last_name: "Runningen", display_name: "Beau R.", level: semi_pro)
+Player.first_or_create!(membership_identifier: 10002, first_name: "Oscar", last_name: "Dominguez", display_name: "Oscar", level: pro)
