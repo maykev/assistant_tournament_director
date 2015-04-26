f1720ed8299157d964760aefb1ae334fd416ed8c
Kevin R. May <pair+kevin@pivotallabs.com>
2015-04-26 01:39:25 -0700

    fix get players

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -2,10 +2,10 @@ class PlayerController < ApplicationController
   def index
     players = {players: []}
 
-    Player.order(:first_name).all do |player|
+    Player.order(:first_name).each do |player|
       players[:players].append({id: player.id, full_name: player.full_name})
     end
-    
+
     render json: players.to_json
   end
 end
