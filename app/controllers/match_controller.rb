0dfc9302ae62bb6681dd3fb1cc42c2f05809986c
Kevin R. May <pair+kevin@pivotallabs.com>
2015-05-01 10:11:08 -0700

    add sponsors and player list api and match creation api

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -12,25 +12,25 @@ class MatchController < ApplicationController
         created_matches = Match.includes(:match_players).where(status: :created)
         created_matches.find_each do |created_match|
           match = {
-            id: created_match.id,
-            players: []
+              id: created_match.id,
+              players: []
           }
 
           match_player_1 = created_match.match_players.order(:id).first
           match_player_2 = created_match.match_players.order(:id).last
 
           player_1 = {
-            id: match_player_1.player.id,
-            full_name: match_player_1.player.full_name,
-            display_name: match_player_1.player.display_name,
-            games_on_the_wire: match_player_1.score
+              id: match_player_1.player.id,
+              full_name: match_player_1.player.full_name,
+              display_name: match_player_1.player.display_name,
+              games_on_the_wire: match_player_1.score
           }
 
           player_2 = {
-            id: match_player_2.player.id,
-            full_name: match_player_2.player.full_name,
-            display_name: match_player_2.player.display_name,
-            games_on_the_wire: match_player_2.score
+              id: match_player_2.player.id,
+              full_name: match_player_2.player.full_name,
+              display_name: match_player_2.player.display_name,
+              games_on_the_wire: match_player_2.score
           }
 
           match[:players].append(player_1)
@@ -44,11 +44,39 @@ class MatchController < ApplicationController
   end
 
   def create
-    match = Match.create!(table_number: params[:table_number], status: :in_progress)
-    MatchPlayer.create!(match: match, player_id: params[:players][0][:id])
-    MatchPlayer.create!(match: match, player_id: params[:players][1][:id])
+    puts "*" * 100
+    puts params.inspect
+    puts "*" * 100
+    match = Match.create!(table_number: params[:table_number], status: :created)
+    player_1 = Player.find(params[:players][0][:id])
+    player_2 = Player.find(params[:players][1][:id])
 
-    render json: { id: match.id }.to_json
+    match_player_1 = MatchPlayer.create!(match: match, player: player_1, score: 9 - player_1.level.games_required)
+    match_player_2 = MatchPlayer.create!(match: match, player: player_2, score: 9 - player_2.level.games_required)
+
+    match_json = {
+        id: match.id,
+        players: []
+    }
+
+    match_player_1_json = {
+        id: match_player_1.player.id,
+        full_name: match_player_1.player.full_name,
+        display_name: match_player_1.player.display_name,
+        games_on_the_wire: match_player_1.score
+    }
+
+    match_player_2_json = {
+        id: match_player_2.player.id,
+        full_name: match_player_2.player.full_name,
+        display_name: match_player_2.player.display_name,
+        games_on_the_wire: match_player_2.score
+    }
+
+    match_json[:players].append(match_player_1_json)
+    match_json[:players].append(match_player_2_json)
+
+    render json: match_json.to_json
   end
 
   def update
