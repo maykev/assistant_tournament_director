5224c1b051757ee7b9529ed959c9979311efd03a
Kevin May <kevin.may@dbnetworks.com>
2016-03-22 23:53:19 -0700

    Add mode to tournaments and implement GET /api/tournaments

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,6 +2,9 @@ class Tournament < ActiveRecord::Base
   extend Enumerize
 
   enumerize :status, in: [:created, :in_progress, :finished]
+  enumerize :mode, in: [:full, :semi]
+
+  serialize :table_numbers, Array
 
   has_many :player_tournaments
   has_many :players, through: :player_tournaments
--- db/migrate/20160316064216_add_table_numbers_to_tournaments.rb
+++ db/migrate/20160316064216_add_table_numbers_to_tournaments.rb
@@ -1,5 +1,5 @@
 class AddTableNumbersToTournaments < ActiveRecord::Migration
   def change
-      add_column :tournaments, :table_numbers, :string
+      add_column :tournaments, :table_numbers, :text
   end
 end
