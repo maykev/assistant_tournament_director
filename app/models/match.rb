10f0c561e75c0e675e7622b5fbf9ab64f5464fdb
Kevin May <kevin.may@dbnetworks.com>
2016-03-27 15:26:39 -0700

    Implement GET /api/matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,9 +1,30 @@
 class Match < ActiveRecord::Base
-  extend Enumerize
+    extend Enumerize
+    enumerize :status, in: [:created, :in_progress, :finished]
 
-  enumerize :status, in: [:created, :in_progress, :finished]
+    belongs_to :tournament
+    has_many :match_players, dependent: :destroy
+    has_many :players, through: :match_players
 
-  belongs_to :tournament
-  has_many :match_players, dependent: :destroy
-  has_many :players, through: :match_players
+    def race
+        match.tournament.matches.where(status: :created).count > 1 ? match.tournament.race : match.tournament.final_race
+    end
+
+    def display_name(position)
+        display_names = []
+        match_players = self.match_players.order(position: :asc)
+
+        if match_players.first.player.first_name != match_players.last.player.first_name
+            display_names = [match_players.first.player.first_name, match_players.last.player.first_name]
+        elsif match_players.first.player.last_name[0] != match_players.last.player.last_name[0]
+            display_names = ["#{match_players.first.player.first_name} #{match_players.first.player.last_name[0]}", "#{match_players.last.player.first_name} #{match_players.last.player.last_name[0]}"]
+        else
+            display_names = ["#{match_players.first.player.first_name[0]}. #{match_players.first.player.last_name}", "#{match_players.last.player.first_name[0]}. #{match_players.last.player.last_name}"]
+        end
+
+        display_names[0] = display_names[0][0..8]
+        display_names[1] = display_names[1][0..8]
+
+        return display_names[position - 1]
+    end
 end
