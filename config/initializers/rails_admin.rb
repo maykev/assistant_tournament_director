08c4bf78c38415796054bea21900582c99f909ec
Kevin May <kevin.may@dbnetworks.com>
2016-05-15 18:38:23 -0700

    Rails admin configuration

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,100 +1,63 @@
 Dir[Rails.root.join('lib', 'rails_admin', '**', '*.rb')].each { |file| require file }
 
 RailsAdmin.config do |config|
-  config.authenticate_with do
-    warden.authenticate! scope: :admin
-  end
-
-  config.authorize_with(:cancan)
-  config.current_user_method(&:current_admin)
-
-  config.actions do
-    dashboard
-    index
-    new
-    export
-    bulk_delete
-    show
-    edit
-    delete
-    show_in_app
-  end
-
-  config.model Admin do
-    list do
-      field :email
+    config.authenticate_with do
+        warden.authenticate! scope: :admin
     end
 
-    show do
-      include_all_fields
+    config.authorize_with(:cancan)
+    config.current_user_method(&:current_admin)
+
+    config.actions do
+        dashboard
+        index
+        new
+        export
+        bulk_delete
+        show
+        edit
+        delete
+        show_in_app
     end
 
-    edit do
-      include_fields :email, :password, :password_confirmation
-    end
-  end
-
-  config.model BracketConfiguration do
-      list do
-          include_all_fields
-      end
-
-      show do
-          include_all_fields
-      end
-
-      edit do
-          include_all_fields
-      end
-  end
-
-  config.model Level do
-    list do
-      include_all_fields
+    config.model Admin do
+        include_fields :email, :password, :password_confirmation
     end
 
-    show do
-      include_all_fields
+    config.model BracketConfiguration do
+        include_all_fields
     end
 
-    edit do
-      include_all_fields
-    end
-  end
-
-  config.model Match do
-    list do
-      include_fields :id, :bracket_position, :match_players
+    config.model Level do
+        include_fields :name, :games_required, :players
     end
 
-    show do
-      include_all_fields
+    config.model Match do
+        list do
+            include_fields :id, :tournament, :bracket_position, :table_number, :match_players
+        end
+
+        show do
+            include_fields :id, :tournament, :bracket_position, :table_number, :match_players
+        end
+
+        edit do
+            include_fields :id, :bracket_position, :table_number
+        end
     end
 
-    edit do
-      include_all_fields
-    end
-  end
-
-  config.model Player do
-    object_label_method :full_name
-
-    list do
-      include_all_fields
+    config.model Player do
+        object_label_method :full_name
+        include_fields :id, :first_name, :last_name, :level
     end
 
-    show do
-      include_all_fields
-    end
+    config.model Tournament do
+        list do
+            include_fields :id, :name, :status, :race, :final_race, :table_numbers, :mode
+        end
 
-    edit do
-      include_all_fields
+        edit do
+            include_fields :id, :name, :status, :race, :final_race, :table_numbers, :mode, :players
+        end
     end
-  end
-
-  config.model Tournament do
-    edit do
-      include_all_fields
-    end
-  end
 end
