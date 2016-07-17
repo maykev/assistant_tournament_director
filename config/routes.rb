791cbcfc7f7b29208d56dc430d89ae9391049e18
Kevin May <kevin.may@dbnetworks.com>
2016-07-16 21:18:27 -0700

    Revert "junior nationals"
    This reverts commit ac1dc6bba330dd9917368f20f09078da2ff88d8b.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     devise_for :admins
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-    root 'match#junior_nationals'
+    root 'match#index'
 
     resources :tournaments, only: [:index, :show]
     get 'tournaments/:id/players' => 'tournaments#players'
