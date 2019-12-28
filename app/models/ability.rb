b663ac92c2b30bef7a43b29e7f8cc7d631e42e6b
The May Family <maykev@Spartacus.local>
2019-12-28 14:28:14 -0800

    Add permissions to add match players

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,17 +7,17 @@ class Ability
     if admin.super_admin?
       can :access, :rails_admin
       can :dashboard
-      can :index, [Admin, BracketConfiguration, Level, Match, Player, Tournament]
-      can :new, [Admin, BracketConfiguration, Level, Match, Player, Tournament]
+      can :index, [Admin, BracketConfiguration, Level, Match, MatchPlayer, Player, Tournament]
+      can :new, [Admin, BracketConfiguration, Level, Match, MatchPlayer, Player, Tournament]
       can :start_tournament, [Tournament]
       can :list_tournament_players, [Tournament]
       can :add_player, [Tournament]
       can :remove_player, [Tournament]
       can :show_in_app, [Tournament]
-      can :edit, [Admin, BracketConfiguration, Level, Match, Player, Tournament]
+      can :edit, [Admin, BracketConfiguration, Level, Match, MatchPlayer, Player, Tournament]
       can :export, [Player]
       can :history, [Admin, Level, Match, Player, Tournament]
-      can :destroy, [Admin, BracketConfiguration, Level, Match, Tournament]
+      can :destroy, [Admin, BracketConfiguration, Level, Match, MatchPlayer, Tournament]
     else
       can :read, :all
     end
